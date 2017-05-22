14a3ebd5d718b0fefdc1ab6fcc1928927e9cf2fb
Methos <jeff@methos.nl>
2017-05-22 08:42:44 +0200

    Unassigned variables rule
    Unassigned variables rule

---
+++ Variables/Test-UnassignedVariables.psm1
@@ -0,0 +1,79 @@
+function Test-UnassignedVariables {
+    [CmdletBinding()]
+    [OutputType([Microsoft.Windows.PowerShell.ScriptAnalyzer.Generic.DiagnosticRecord[]])]
+    param (
+        [Parameter(
+            Mandatory=$true,
+            Position=1
+        )]
+        [ValidateNotNullOrEmpty()]
+        [System.Management.Automation.Language.ScriptBlockAst]$ScriptBlockAst
+    )
+
+    process
+    {
+        [string[]]$AutomaticVariables = (Get-Variable).name
+        try
+        {
+			$ParametersAst = $ScriptBlockAst.FindAll( { $args[0] -is [System.Management.Automation.Language.ParamBlockAst] }, $true )
+            $VariablesAssignmentAst = $ScriptBlockAst.FindAll( { $args[0] -is [System.Management.Automation.Language.AssignmentStatementAst] }, $true )
+
+            $Parameters = $ParametersAST.Parameters | Select-Object @{l='Name';e={$_.Name.VariablePath.UserPath}}, @{l='Line';e={$_.Extent.StartLineNumber}}
+            $Variables = $variablesAssignmentAst | Select-Object @{l='Name';e={$_.Left.ToString().TrimStart('$')}}, @{l='Line';e={$_.Extent.StartLineNumber}}
+
+            $Assigned = New-Object System.Collections.ArrayList
+            foreach ($Parameter in $Parameters)
+            {
+                $Add = @{
+                    'Name' = $Parameter.Name.ToString()
+                    'Line' = [int]$Parameter.Line.ToString()
+                }
+                $null = $Assigned.Add( ( New-Object PSObject -Property $Add ) )
+            }
+            foreach ($Variable in $Variables)
+            {
+                $Add = @{
+                    'Name' = $var.Name.ToString()
+                    'Line' = [int]$var.Line.ToString()
+                }
+                $null = $Assigned.Add(( New-Object PSObject -Property $Add ))
+            }
+            $Assigned = $Assigned | Sort-Object -Property Line
+
+            $ExpressionsAst = $ScriptBlockAst.FindAll( { $args[0] -is [System.Management.Automation.Language.VariableExpressionAst] }, $true )
+            $Expressions = $ExpressionsAst | Select-Object @{n='Name';e={$_.VariablePath.Userpath}}, @{l='Line';e={$_.Extent.StartLineNumber}}, Extent
+
+            foreach ($Expression in $Expressions )
+            {
+                if (( $Expression.Name.ToString() -notin $Assigned.Name ) -and ( $Expression.Name.ToString() -notin $AutomaticVariables ))
+                {
+                    [Microsoft.Windows.PowerShell.ScriptAnalyzer.Generic.DiagnosticRecord]@{
+					    Message  = "$($Expression.Name) is used before it is assigned a value"
+					    Extent   = $Expression.Extent
+					    RuleName = 'PSDoNotUseUnassignedVariables'
+					    Severity = 'Warning'
+				    }
+                }
+                else
+                {
+                    $Line = [int]$Exp.Line.ToString()
+                    $AssignsAfter = @($Assigned | Where-Object { ( $_.Name -eq $Expression.Name.ToString() ) -and ( $_.Line -gt $Line ) })
+                    $AssignsBefore = @($Assigned | Where-Object { ( $_.Name -eq $Expression.Name.ToString() ) -and ( $_.Line -lt $Line ) })
+
+                    if ( ( -not $AssignsBefore ) -and ( $AssignsAfter ) )
+                    {
+                        [Microsoft.Windows.PowerShell.ScriptAnalyzer.Generic.DiagnosticRecord]@{
+					        Message  = "$($Expression.Name) is used before it is assigned a value"
+					        Extent   = $Expression.Extent
+					        RuleName = 'PSDoNotUseUnassignedVariables'
+					        Severity = 'Warning'
+				        }
+                    } 
+                }
+            }
+        }
+        catch {
+            $PSCmdlet.ThrowTerminatingError( $_ )
+        }
+    }
+}
