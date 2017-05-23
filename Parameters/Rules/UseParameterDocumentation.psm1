1b7812887385471151a77346325598b04383edf2
Methos <jeff@methos.nl>
2017-05-23 09:54:37 +0200

    UseParameterDocumentation.psm1

--- Parameters/Rules/UseParameterDocumentation.psm1
+++ Parameters/Rules/UseParameterDocumentation.psm1
@@ -1,4 +1,4 @@
-﻿function Measure-ParameterDocumentation
+﻿ function Measure-ParameterDocumentation
 {
     [CmdletBinding()]
     [OutputType([Microsoft.Windows.PowerShell.ScriptAnalyzer.Generic.DiagnosticRecord[]])]
@@ -9,141 +9,30 @@
         [System.Management.Automation.Language.ScriptBlockAst]
         $ScriptBlockAst
     )
-
-    begin 
-    {
-    }
-
     Process
     {
         $results = @()
         try
         {
-            # Finds param block
-            [ScriptBlock]$ParamBlockPredicate = {
-                param 
-                (
-                    [System.Management.Automation.Language.Ast]$Ast
-                )
-                [bool]$ReturnValue = $false
-
-                if ($Ast -is [System.Management.Automation.Language.ParamBlockAst])
-                {
-
-                    $ReturnValue = $true;
-
-                }
-                return $ReturnValue
-            }
-
-            # Finds dynamicparam block
-            [ScriptBlock]$DynamicParamBlockPredicate = {
-                param 
-                (
-                    [System.Management.Automation.Language.Ast]$Ast
-                )
-                [bool]$ReturnValue = $false
-
-                if ($Ast -is [System.Management.Automation.Language.NamedBlockAst])
-                {
-                    [System.Management.Automation.Language.NamedBlockAst]$NamedBlockAst = $Ast
-
-                    if ($NamedBlockAst.BlockKind -eq [System.Management.Automation.Language.TokenKind]::Dynamicparam)
-                    {
-                        $ReturnValue = $true;
-                    }
-
-                }
-                return $ReturnValue
-            }
-
-            # Finds command element block
-            [ScriptBlock]$PipelineAstPredicate = {
-                param 
-                (
-                    [System.Management.Automation.Language.Ast]$Ast
-                )
-                [bool]$ReturnValue = $false
-
-                if ($Ast -is [System.Management.Automation.Language.PipelineAst])
-                {
-                    $ReturnValue = $true;
-                }
-                return $ReturnValue
-            }
-
-            # Finds function block
-            [ScriptBlock]$FunctionPredicate = {
-                param 
-                (
-                    [System.Management.Automation.Language.Ast]$Ast
-                )
-                [bool]$ReturnValue = $false
-
-                if ($Ast -is [System.Management.Automation.Language.FunctionDefinitionAst])
-                {
-                    $ReturnValue = $true;
-                }
-                return $ReturnValue
-            }
-
-
-            [System.Management.Automation.Language.Ast[]]$FunctionBlockAsts = $ScriptBlockAst.FindAll($FunctionPredicate, $true)
-
-            foreach ($Ast in $FunctionBlockAsts)
+            $FunctionsAst = $ScriptBlockAst.FindAll( { $args[0] -is [System.Management.Automation.Language.FunctionDefinitionAst] }, $true )
+            foreach ($FunctionAst in $FunctionsAst)
             {
-                [System.Management.Automation.Language.FunctionDefinitionAst]$FunctionAst = $Ast;
-
-                # get parameters in help already
-                [System.Management.Automation.Language.CommentHelpInfo]$Help = $FunctionAst.GetHelpContent()
-                $ParametersInHelp = $Help.Parameters.Keys
-
-
-                $ParametersInFunction = @()
-
-                # get static params
-                [System.Management.Automation.Language.Ast[]]$ParamBlockAsts = $FunctionAst.FindAll($ParamBlockPredicate,$true)
-
-                foreach ($Ast2 in $ParamBlockAsts)
+                $ParametersAst = $FunctionAst.FindAll( {$args[0] -is [System.Management.Automation.Language.ParamBlockAst]}, $true)
+                $Parameters = $ParametersAst.parameters.name.ToString() | ForEach-Object { $_.replace('$','')}
+                $HelpParameters = $ScriptBlockAst.GetHelpContent().Parameters.Keys
+                foreach ($Parameter in $Parameters)
                 {
-                    [System.Management.Automation.Language.ParamBlockAst]$ParamBlockAst = $Ast2
-
-                    foreach ($ParamAst in $ParamBlockAst.Parameters)
+                    if ($HelpParameters -notcontains $Parameter)
                     {
-                        $ParametersInFunction += $ParamAst.Name.VariablePath.UserPath
-                    }
-                }
-
-                # get dynamic params
-                [System.Management.Automation.Language.Ast[]]$DynamicParamBlockAsts = $FunctionAst.FindAll($DynamicParamBlockPredicate,$true)
-
-                foreach ($Ast3 in $DynamicParamBlockAsts)
-                {
-                    $Script = $Ast3.statements -join "`n"
-                    $DynamicParamBlockScriptBlock = [ScriptBlock]::Create($script)
-                    $RuntimeDictionary = Invoke-Command -Scriptblock $DynamicParamBlockScriptBlock
-                    foreach ($Param in $RuntimeDictionary.Keys)
-                    {
-                        $ParametersInFunction += $Param
-                    }
-
-                }
-
-                # check params against help params              
-                foreach ($FunctionParam in $ParametersInFunction)
-                {
-                    if ($null -eq $ParametersInHelp -or $ParametersInHelp -inotcontains $FunctionParam)
-                    {
-                        $Result = [Microsoft.Windows.PowerShell.ScriptAnalyzer.Generic.DiagnosticRecord]@{"Message"  = "Function missing .PARAMETER documentation for parameter '$FunctionParam'"; 
-                                                    "Extent"   = $Ast.Extent;
-                                                    "RuleName" = $PSCmdlet.MyInvocation.MyCommand.Name.Replace("Measure-","Use");
-                                                    "Severity" = "Warning"}
-                        $Results += $Result    
+                        [Microsoft.Windows.PowerShell.ScriptAnalyzer.Generic.DiagnosticRecord]@{
+                            "Message"  = "Unknown parameter $Parameter documented in help"; 
+                            "Extent"   = $Function.Extent;
+                            "RuleName" = $PSCmdlet.MyInvocation.MyCommand.Name.Replace("Measure-","Use");
+                            "Severity" = "Warning"
+                        }
                     }
                 }
             }
-
-            return $results
         }
         catch
         {
