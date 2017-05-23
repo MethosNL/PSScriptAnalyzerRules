8f948f80bb7866cf9e78a469e1e24d9c508e8056
Methos <jeff@methos.nl>
2017-05-23 11:06:03 +0200

    UseFunctionNamingConvention
    UseFunctionNamingConvention

---
+++ Functions/Rules/UseFunctionNamingConvention.psm1
@@ -0,0 +1,38 @@
+﻿function Measure-FunctionNamingConvention
+{
+    [CmdletBinding()]
+    [OutputType([Microsoft.Windows.PowerShell.ScriptAnalyzer.Generic.DiagnosticRecord[]])]
+    Param
+    (
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [System.Management.Automation.Language.ScriptBlockAst]
+        $ScriptBlockAst
+    )
+    Process
+    {
+        $results = @()
+        try
+        {
+            $FunctionsAst = $ScriptBlock.FindAll( { $args[0] -is [System.Management.Automation.Language.FunctionDefinitionAst] }, $true )
+            foreach ($FunctionAst in $FunctionsAst)
+            {
+                if ($FunctionAst.Name -match '^([a-zA-Z]+)-([A-Z]{1,1})([a-zA-Z0-9])+')
+                {}
+                else
+                {
+                    [Microsoft.Windows.PowerShell.ScriptAnalyzer.Generic.DiagnosticRecord]@{
+                        "Message"  = "Incorrectly named function based on cmdlet naming conventions"; 
+                        "Extent"   = $Function.Extent;
+                        "RuleName" = $PSCmdlet.MyInvocation.MyCommand.Name.Replace("Measure-","Use");
+                        "Severity" = "Warning"
+                    }
+                }
+            }
+        }
+        catch
+        {
+            $PSCmdlet.ThrowTerminatingError($PSItem)
+        }
+    }
+}
